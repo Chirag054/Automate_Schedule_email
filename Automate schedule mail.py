3011a01ea8133fe2efdb92f8120fe2b5a2d6a869
Chirag054 <68994357+Chirag054@users.noreply.github.com>
2022-05-16 16:04:26 +0530

    Update and rename Automate schedule mail.txt to Automate schedule mail.py

--- Automate schedule mail.py
+++ Automate schedule mail.py
@@ -34,15 +34,3 @@ def sendMail():
         print(e)
 
 
-# Scheduling the Mail -----------
-
-from apscheduler.schedulers.blocking import BlockingScheduler
-from run import sendMail
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
-def scheduled_job():
-    print('This job is run every weekday at 8 AM.')
-    sendMail()
-sched.start()
